6ac8415b73a92790e10ea28ca6d7274786d3134d
Duy An Ngo Nguyen <40843946+ZoroZero@users.noreply.github.com>
2020-09-04 13:25:46 +0700

    Add config module for env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,19 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccountsModule } from './accounts/accounts.module';
-
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 @Module({
-  imports: [TypeOrmModule.forRoot(), AuthModule, AccountsModule, UsersModule],
+  imports: [TypeOrmModule.forRoot(), AuthModule, AccountsModule, UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+      validationSchema: Joi.object({
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION_TIME: Joi.string().required()
+      })
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
