8caaf3652d9a872d24b45d2382a76dfbead984db
Duy An Ngo Nguyen <40843946+ZoroZero@users.noreply.github.com>
2020-09-24 21:13:28 +0700

    Deploy attend 8

--- ormconfig.js
+++ ormconfig.js
@@ -22,7 +22,7 @@
 
 module.exports = {
   type: process.env.type,
-  host: "ADMIN\\SQLEXPRESS",
+  host: process.env.host,
   port: parseInt(process.env.port),
   database: process.env.database,
   username: process.env.usernamedb,
